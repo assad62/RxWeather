178b1af86a042b23dfd710a4165a81ba125d79b4
Mohammad Assad <assad62@hotmail.com>
2022-05-01 10:57:06 +0800

    initial commit

---
+++ RxWeather/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  UIKitTest
+//
+//  Created by Development on 19/4/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+       
+    }
+    
+    func setupViews(){
+        
+    }
+    
+    
+    
+    
+   
+}
+
